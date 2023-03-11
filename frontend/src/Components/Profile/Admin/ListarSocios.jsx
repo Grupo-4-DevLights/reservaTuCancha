71b779878c50c9d05b2aa3e901827c98d4ad8fa7
Gastón Acosta <gastoneacosta@outlook.com>
2023-03-11 18:09:47 -0300

    minor fixes

--- frontend/src/Components/Profile/Admin/ListarSocios.jsx
+++ frontend/src/Components/Profile/Admin/ListarSocios.jsx
@@ -1,71 +1,77 @@
-import React, { useEffect, useState } from 'react'
-import { modifyUsuer, obtenerSocios, obtenerSociosConId } from '../../../Services/Admin';
-import TableLayout from '../TableLayout';
-import { FormEditUser } from './FormEditUser';
-
+import React, { useEffect, useState } from "react";
+import {
+  modifyUsuer,
+  obtenerSocios,
+  obtenerSociosConId,
+} from "../../../Services/Admin";
+import TableLayout from "../TableLayout";
+import { FormEditUser } from "./FormEditUser";
 
 const initialUser = {
-    id: "",
-    nombre: "",
-    email: "",
-    rol: "",
-  };
+  id: "",
+  nombre: "",
+  email: "",
+  rol: "",
+};
 
 export function ListarSocios() {
-    const [socios, setSocios] = useState([]);
-    const [user, setUser]  = useState(initialUser);
-    const [modify, setModify] = useState(false);
+  const [socios, setSocios] = useState([]);
+  const [user, setUser] = useState(initialUser);
+  const [modify, setModify] = useState(false);
 
-    useEffect(()=>{
-        obtenerSocios()
-        .then((data)=>{
-            setSocios(data)
-        }).catch((error)=>{
-            console.log(error)
-        })
-    },[modify])
 
-    const handleEditUser = (row) => {
-        setUser(row);
-    };
+  useEffect(() => {
+    obtenerSocios()
+      .then((data) => {
+        setSocios(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [modify]);
 
-    const handleSaveUser = (values) => {
-        modifyUsuer(values).then(
-            (data)=>{
-                console.log("Usuario Modificado", data);
-                setModify(!modify);
-            }
-        )
-        setUser(initialUser);
+  const handleEditUser = (row) => {
+    setModify(true)
+    setUser(row);
+  };
 
-    };
+  const handleSaveUser = (values) => {
+    modifyUsuer(values).then((data) => {
+      console.log("Usuario Modificado", data);
+      setModify(!modify);
+    });
+    setUser(initialUser);
+  };
 
-    const handleDeleteUser = (value) =>{
-        console.log(value)
-    }
+  const handleDeleteUser = (value) => {
+    console.log(value);
+  };
 
   return (
     <>
-        
-        <div className="flex flex-col ">
-              <div className="flex justify-center  text-center text-3xl font-bold w-full mb-8">
-                  <h1>Listar Usuarios</h1>
-              </div>
-              <div className="flex">
-                  <TableLayout
-                      data={socios}
-                      onEditUser={handleEditUser}
-                      onDeleteUser={handleDeleteUser}
-                  />
-              </div>
-              <div className="flex justify-center mt-8 text-center text-3xl font-bold w-full mb-8">
-                  <h1>Modificar Usuario</h1>
-              </div>
-              <div className="flex">
-                   <FormEditUser onSaveUser={handleSaveUser} user={user}/> 
-              </div>
+      <div className="flex flex-col ">
+        <div className="flex justify-center  text-center text-3xl font-bold w-full mb-8">
+          <h1>Listar Usuarios</h1>
         </div>
-        
+        <div className="flex">
+          <TableLayout
+            data={socios}
+            onEditUser={handleEditUser}
+            onDeleteUser={handleDeleteUser}
+          />
+        </div>
+
+        {modify && (
+          <>
+            <div className="flex justify-center mt-8 text-center text-3xl font-bold w-full mb-8">
+              <h1>Modificar Usuario</h1>
+            </div>
+            <div className="flex">
+              <FormEditUser onSaveUser={handleSaveUser} user={user} />
+            </div>
+          </>
+        )}
+      </div>
     </>
-  )
+  );
 }
--- frontend/src/Components/Profile/TableLayout.jsx
+++ frontend/src/Components/Profile/TableLayout.jsx
@@ -1,31 +1,47 @@
-import React from 'react'
-import ButtonsAdmin from './Admin/ButtonsAdmin';
+import React from "react";
+import ButtonsAdmin from "./Admin/ButtonsAdmin";
 
-export default function TableLayout({data, onEditUser, onDeleteUser}) {
-  
-    return (
-      <>
-        {data && data.length > 0  && (
-          <table className='border-collapse table-fixed w-full text-sm text-center'>
-            <thead>
-              <tr className='text-xl'>
-                {Object.keys(data[0]).map((title) => (
-                  <th key={title}>{title}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {data.map((row) => (
-                <tr key={row.nombre} className="text-lg">
-                  {Object.values(row).map((item) => (
-                    <td>{item}</td>
-                  ))}
-                  <td><button className="bg-red-500" onClick={() => onEditUser(row)}>Modificar</button><a className="bg-red-500 mx-2" onClick={()=> onDeleteUser(row)}>Eliminar</a></td>
-                </tr>
+export default function TableLayout({ data, onEditUser, onDeleteUser }) {
+  return (
+    <>
+      {data && data.length > 0 && (
+        <table className="border-collapse table-fixed w-full text-sm text-center">
+          <thead>
+            <tr className="text-xl">
+              {Object.keys(data[0]).map((title, index) => (
+                <th key={`${title}_${index}`}>{title}</th>
               ))}
-            </tbody>
-          </table>
-        )}
-      </>
-    );
-  }
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((row, index) => (
+              <React.Fragment key={index}>
+                {row.rol === "administrador" ? null : (
+                  <tr className="text-lg">
+                    {Object.values(row).map((item, index) => (
+                      <td key={index}>{item}</td>
+                    ))}
+                    <td>
+                      <button
+                        className="bg-red-500"
+                        onClick={() => onEditUser(row)}
+                      >
+                        Modificar
+                      </button>
+                      <a
+                        className="bg-red-500 mx-2"
+                        onClick={() => onDeleteUser(row)}
+                      >
+                        Eliminar
+                      </a>
+                    </td>
+                  </tr>
+                )}
+              </React.Fragment>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
+}
