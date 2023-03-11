bf51e5e8d7780a706b3b0be3ffdd063340e0c811
Gastón Acosta <gastoneacosta@outlook.com>
2023-03-11 16:01:20 -0300

    Update Aside.jsx

--- frontend/src/Components/Profile/Aside.jsx
+++ frontend/src/Components/Profile/Aside.jsx
@@ -8,7 +8,7 @@ export  function Aside() {
     <div className="mb-3">
         {user && (
             <>
-            <h1 className="text-WHITE text-2xl font-sans px-2 py-1 rounded-lg flex">
+            <h1 className="font-sans px-2 py-1 rounded-lg flex">
                 Bienvenido, <p className="capitalize ml-2">{user.nombre}.</p>
             </h1>
             <p className='px-2 py-1 capitalize'>{user.rol}</p>
