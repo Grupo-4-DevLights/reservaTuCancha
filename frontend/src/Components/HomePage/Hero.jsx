6d88081743f05b224ca5157dd90279036366e520
Gastón Acosta <gastoneacosta@outlook.com>
2023-03-10 21:59:08 -0300

    hero

--- frontend/src/Components/HomePage/Hero.jsx
+++ frontend/src/Components/HomePage/Hero.jsx
@@ -1,7 +1,7 @@
 export function Hero(){
   return(
       <div>
-      <div className="sm:grid grid-cols-2 items-center bg-gradient-to-tr from-emerald-800 to-emerald-400 h-[88vh]">
+      <div className="sm:grid grid-cols-2 items-center bg-gradient-to-tr from-emerald-800 to-emerald-400 h-[95vh]">
       <div className="flex justify-center items-center flex-col">
         <div className="w-[70%] mt-5 mb-5">
           <h1 className="text-5xl text-white mb-5 hidden sm:flex font-sans font-bold">Reserva tu cancha😎</h1>
